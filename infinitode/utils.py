361ae02d33427119d304a83b8b70dd031b8145e9
Sprylos <sprylos.discord@gmail.com>
2022-08-20 16:31:56 +0200

    Add try_int util

--- infinitode/utils.py
+++ infinitode/utils.py
@@ -3,14 +3,24 @@ from __future__ import annotations
 # std
 from typing import Any
 
-__all__ = ('MISSING',)
+__all__ = (
+    'MISSING',
+    'try_int'
+)
+
+
+def try_int(string: str, /, *, default: int = 0) -> int:
+    try:
+        return int(string)
+    except ValueError:
+        return default
 
 
 class _MissingSentinel:
     # taken from discord.py, see:
     # https://github.com/Rapptz/discord.py/blob/a14b43f2fda863ed6555374eb872bf014bdd1adf/discord/utils.py#L96
 
-    def __eq__(self, other):
+    def __eq__(self, other: Any):
         return False
 
     def __bool__(self):
